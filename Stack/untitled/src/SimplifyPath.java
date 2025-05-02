651d5d5e489f668e67fefaa36d93338bf20d0b83
Mahbub <mahbub.buet.cse18@gmail.com>
2025-05-02 11:55:04 +0600

    SimplifyPath is added to Stack

---
+++ Stack/untitled/src/SimplifyPath.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+class Solution {
+    public String simplifyPath(String path) {
+        String[] arr = path.split("/");
+        Stack<String> stack = new Stack<>();
+        for(int i=0;i<arr.length;i++)
+        {
+            if(!arr[i].isEmpty())
+            {
+                if(!arr[i].equals(".."))
+                {
+                    stack.push(arr[i]);
+                }
+                else
+                {
+                    if(!stack.isEmpty())
+                    {
+                        stack.pop();
+                    }
+                }
+            }
+        }
+        StringBuilder result = new StringBuilder();
+        for(String c: stack)
+        {
+            System.out.println(c);
+            result.append("/").append(c);
+        }
+        if(result.length()==0)
+        {
+            result.append("/");
+        }
+        return result.toString();
+    }
+}
+
+public class SimplifyPath {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a UNIX-style path: ");
+        String inputPath = scanner.nextLine();
+
+        Solution solution = new Solution();
+        String simplified = solution.simplifyPath(inputPath);
+
+        System.out.println("Simplified Path: " + simplified);
+        scanner.close();
+    }
+}
