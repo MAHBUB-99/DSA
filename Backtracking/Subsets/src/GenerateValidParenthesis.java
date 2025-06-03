53bdfa36a038a83989f203b65aab830df91f1c02
Mahbub <mahbub.buet.cse18@gmail.com>
2025-06-03 12:11:13 +0600

    Generate Valid Parenthesis

---
+++ Backtracking/Subsets/src/GenerateValidParenthesis.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateValidParenthesis {
+    List<String> result = new ArrayList<>();
+
+    public void generate(String ans, int max, int open, int close) {
+        if (ans.length() == max * 2) {
+            result.add(ans);
+            return;
+        }
+        if (open < max)
+            generate(ans + "(", max, open + 1, close);
+        if (close < open)
+            generate(ans + ")", max, open, close + 1);
+    }
+
+    public List<String> generateParenthesis(int n) {
+        generate("", n, 0, 0);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        GenerateValidParenthesis obj = new GenerateValidParenthesis();
+        int n = 3;
+        List<String> combinations = obj.generateParenthesis(n);
+
+        System.out.println("Valid Parentheses Combinations for n = " + n + ":");
+        for (String s : combinations) {
+            System.out.println(s);
+        }
+    }
+}
