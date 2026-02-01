9685f5b7cf05967b8796aaa7de626ae1cc8c6a1c
Mahbub <mahbub.buet.cse18@gmail.com>
2026-02-01 22:27:33 +0600

    Linkedlist reverse 2 added

---
+++ LinkedList/LinkedList/ClassLibrary1/ReverseLinkedList2.cs
@@ -0,0 +1,87 @@
+﻿namespace ReverseLinkedList2
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class ReverseLinkedList2
+    {
+        public ListNode ReverseBetween(ListNode head, int left, int right)
+        {
+            ListNode curr = head;
+            ListNode prev = head;
+            int count = 1;
+            while (count < left)
+            {
+                prev = curr;
+                curr = curr.next;
+                count++;
+            }
+            ListNode start = curr;
+            Console.WriteLine(start.val);
+            while (count < right)
+            {
+                start = start.next;
+                count++;
+            }
+            ListNode nextt = start.next;
+            if (nextt != null) Console.WriteLine(nextt.val);
+
+            start.next = null;
+            ListNode neww = Reverse(prev.next);
+            if (neww != null) Console.WriteLine(neww.val);
+
+            prev.next = neww;
+            start.next = nextt;
+            return head;
+        }
+
+        private ListNode Reverse(ListNode head)
+        {
+            ListNode curr = head;
+            ListNode prev = null;
+            while (curr != null)
+            {
+                ListNode temp = curr.next;
+                curr.next = prev;
+                prev = curr;
+                curr = temp;
+            }
+            return prev;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Build linked list: 1 -> 2 -> 3 -> 4 -> 5
+            ListNode head = new ListNode(1,
+                new ListNode(2,
+                new ListNode(3,
+                new ListNode(4,
+                new ListNode(5)))));
+
+            ReverseLinkedList2 solver = new ReverseLinkedList2();
+            head = solver.ReverseBetween(head, 2, 4);
+
+            // Print result
+            PrintList(head);
+        }
+
+        static void PrintList(ListNode head)
+        {
+            while (head != null)
+            {
+                Console.Write(head.val + " ");
+                head = head.next;
+            }
+        }
+    }
+}
---
+++ LinkedList/LinkedList/ReverseLinkedList2/Program.cs
@@ -0,0 +1,69 @@
+﻿class Program
+{
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+    public static ListNode ReverseBetween(ListNode head, int left, int right)
+    {
+        ListNode result = new ListNode(0);
+        result.next = head;
+        ListNode prev = result;
+        int count = 1;
+        while (count < left)
+        {
+            prev = prev.next;
+            count++;
+        }
+        ListNode start = prev.next;
+        ListNode curr = start;
+        while (count < right)
+        {
+            curr = curr.next;
+            count++;
+        }
+        ListNode nextt = curr.next;
+        curr.next = null;
+
+        ListNode neww = Reverse(prev.next);
+
+        prev.next = neww;
+        start.next = nextt;
+        return result.next;
+    }
+    private static ListNode Reverse(ListNode head)
+    {
+        ListNode curr = head;
+        ListNode prev = null;
+        while (curr != null)
+        {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+    static void Main()
+    {
+        ListNode head = new ListNode(1,
+            new ListNode(2,
+            new ListNode(3,
+            new ListNode(4,
+            new ListNode(5)))));
+
+        ReverseBetween(head, 2, 4);
+
+        while (head != null)
+        {
+            Console.Write(head.val + " ");
+            head = head.next;
+        }
+    }
+}
